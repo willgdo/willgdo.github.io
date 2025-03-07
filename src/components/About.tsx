38683fc13faa9bae75d76c2ed2ec77b9c50dea8a
Willian Oliveira <willian.oliveira737@gmail.com>
2025-03-06 22:12:47 -0300

    Add estrutura para informacoes

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,8 +1,16 @@
+import { useState } from "react";
+
 interface AboutProps {
   isReduced: boolean;
 }
 
 function About({ isReduced }: AboutProps) {
+  const [title, setTitle] = useState("Sobre");
+
+  function handleContent(newTitle: string) {
+    setTitle(newTitle);
+  }
+
   return (
     <>
       <section className={`about ${isReduced ? "a-expanded" : ""}`}>
@@ -10,14 +18,80 @@ function About({ isReduced }: AboutProps) {
           <div className="nav-wrapper">
             <nav>
               <ul>
-                <li>Sobre</li>
-                <li>Formação</li>
-                <li>Experiência</li>
-                <li>Contato</li>
+                <li onClick={() => handleContent("Sobre")}>Sobre</li>
+                <li onClick={() => handleContent("Formação")}>Formação</li>
+                <li onClick={() => handleContent("Experiência")}>
+                  Experiência
+                </li>
+                <li onClick={() => handleContent("Contato")}>Contato</li>
               </ul>
             </nav>
           </div>
-          <div>Conteúdo</div>
+          <div className="content-info">
+            {title === "Sobre" && (
+              <>
+                <h3>{title}</h3>
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Corporis consectetur dolore reiciendis consequatur mollitia,
+                  iure, molestias soluta quia rem architecto fugit, temporibus
+                  vitae atque natus quas totam doloribus unde! Quaerat. Lorem
+                  ipsum dolor, sit amet consectetur adipisicing elit. Harum
+                  quasi totam repellat maiores beatae modi molestias iusto
+                  voluptatem ducimus illum tempore reprehenderit odio quos,
+                  eligendi architecto! Commodi cupiditate ipsam impedit!
+                </p>
+              </>
+            )}
+
+            {title === "Formação" && (
+              <>
+                <h3>{title}</h3>
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Corporis consectetur dolore reiciendis consequatur mollitia,
+                  iure, molestias soluta quia rem architecto fugit, temporibus
+                  vitae atque natus quas totam doloribus unde! Quaerat. Lorem
+                  ipsum dolor, sit amet consectetur adipisicing elit. Harum
+                  quasi totam repellat maiores beatae modi molestias iusto
+                  voluptatem ducimus illum tempore reprehenderit odio quos,
+                  eligendi architecto! Commodi cupiditate ipsam impedit!
+                </p>
+              </>
+            )}
+
+            {title === "Experiência" && (
+              <>
+                <h3>{title}</h3>
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Corporis consectetur dolore reiciendis consequatur mollitia,
+                  iure, molestias soluta quia rem architecto fugit, temporibus
+                  vitae atque natus quas totam doloribus unde! Quaerat. Lorem
+                  ipsum dolor, sit amet consectetur adipisicing elit. Harum
+                  quasi totam repellat maiores beatae modi molestias iusto
+                  voluptatem ducimus illum tempore reprehenderit odio quos,
+                  eligendi architecto! Commodi cupiditate ipsam impedit!
+                </p>
+              </>
+            )}
+
+            {title === "Contato" && (
+              <>
+                <h3>{title}</h3>
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Corporis consectetur dolore reiciendis consequatur mollitia,
+                  iure, molestias soluta quia rem architecto fugit, temporibus
+                  vitae atque natus quas totam doloribus unde! Quaerat. Lorem
+                  ipsum dolor, sit amet consectetur adipisicing elit. Harum
+                  quasi totam repellat maiores beatae modi molestias iusto
+                  voluptatem ducimus illum tempore reprehenderit odio quos,
+                  eligendi architecto! Commodi cupiditate ipsam impedit!
+                </p>
+              </>
+            )}
+          </div>
           <footer className="footer">By Will</footer>
         </div>
       </section>
