55df5cba77c219316ce7855c19acdef1bdfe97ab
Willian Oliveira <willian.oliveira737@gmail.com>
2025-03-14 00:41:32 -0300

    Add estilos e informaçoes de experiencia

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -122,16 +122,65 @@ function About({ isReduced }: AboutProps) {
             {title === "Experiência" && (
               <>
                 <h3>{title}</h3>
-                <p>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Corporis consectetur dolore reiciendis consequatur mollitia,
-                  iure, molestias soluta quia rem architecto fugit, temporibus
-                  vitae atque natus quas totam doloribus unde! Quaerat. Lorem
-                  ipsum dolor, sit amet consectetur adipisicing elit. Harum
-                  quasi totam repellat maiores beatae modi molestias iusto
-                  voluptatem ducimus illum tempore reprehenderit odio quos,
-                  eligendi architecto! Commodi cupiditate ipsam impedit!
-                </p>
+
+                <div className="exp">
+                  <div>
+                    <img
+                      src="https://media.licdn.com/dms/image/v2/C4D0BAQH2vcrgNsIdJg/company-logo_100_100/company-logo_100_100/0/1630509285586/visto_tecnologia_logo?e=1746662400&v=beta&t=5r4SrE_Zu4NkiPXEkC7LnFhs28V4Ii0nars3p-HrAh4"
+                      alt="logo visto"
+                    />
+                  </div>
+
+                  <div>
+                    <strong>
+                      Visto Tecnologia - Senior Software Developer
+                    </strong>
+                    <br />
+                    <span>
+                      Mar/2017 - Dez/2024 (7 anos e 9 meses) | São Paulo |
+                      Remoto
+                    </span>
+                    <br />
+                  </div>
+                </div>
+
+                <div className="exp">
+                  <div>
+                    <img
+                      src="https://media.licdn.com/dms/image/v2/C4D0BAQEl9FKtIuJ7tQ/company-logo_100_100/company-logo_100_100/0/1631373459452/santa_transmedia_logo?e=1746662400&v=beta&t=6uZ7FZtZNV7OgO-1tVzJN03aYDV8hnlbkbNXSwH46aU"
+                      alt="logo santa transmedia"
+                    />
+                  </div>
+
+                  <div>
+                    <strong>Santa Transmedia - Web Developer</strong>
+                    <br />
+                    <span>
+                      Jul/2016 - Jan/2017 (7 meses) | São Paulo | Presencial
+                    </span>
+                    <br />
+                  </div>
+                </div>
+
+                <div className="exp">
+                  <div>
+                    <img
+                      src="https://media.licdn.com/dms/image/v2/C560BAQEyA2WbdnCdxw/company-logo_200_200/company-logo_200_200/0/1631357099395?e=1746662400&v=beta&t=1jqXwKbH5KncKr5EjvR2RVuy0ncsD23mnweCT4GR_So"
+                      alt="logo avelor"
+                    />
+                  </div>
+
+                  <div>
+                    <strong>
+                      Avelor Tecnologia da Informação - Web Developer (estágio)
+                    </strong>
+                    <br />
+                    <span>
+                      Out/2015 - Jul/2016 (9 meses) | São Paulo | Presencial
+                    </span>
+                    <br />
+                  </div>
+                </div>
               </>
             )}
 
@@ -201,8 +250,13 @@ function About({ isReduced }: AboutProps) {
           <footer className="footer">By Will</footer>
         </div>
       </section>
-      <a className="btn-download-cv" href="#" download>
-        <img src="./download.png" alt="download CV" title="download CV" />
+      <a
+        className={`${isReduced ? "btn-download-cv" : "hidden"}`}
+        href="#"
+        download
+      >
+        <img src="./download.png" alt="download CV" />
+        <span>Baixar CV</span>
       </a>
     </>
   );
