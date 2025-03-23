1ee603cdb90ccd6ec1d5f783930d0ecf442b60e5
Willian Oliveira <willian.oliveira737@gmail.com>
2025-03-23 12:00:00 -0300

    Adiciona rotas para Home

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import { AppRoutes } from "./routes/appRoutes";
 import "./styles/index.css";
-import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   </StrictMode>
 );
---
+++ src/pages/Home.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import Header from "../components/Header.tsx";
+import Perfil from "../components/Perfil.tsx";
+
+export function Home() {
+  const [isReduced, setIsReduced] = useState(false);
+
+  function handleClick() {
+    setIsReduced(true);
+  }
+
+  return (
+    <div className="container">
+      <Header isReduced={isReduced} onClick={handleClick} />
+      <Perfil isReduced={isReduced} />
+    </div>
+  );
+}
