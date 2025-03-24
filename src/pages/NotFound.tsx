f08dabbe6cc97cb00bfe78653d18a15d3789068b
Willian Oliveira <willian.oliveira737@gmail.com>
2025-03-24 12:00:00 -0300

    Adiciona rotas para NotFound

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,14 @@
+export function NotFound() {
+  return (
+    <div className="notFound">
+      <p>This page doesn't exist bro! What are you doing here?</p>
+      <img
+        src="https://media.tenor.com/xIkBRAE3zVgAAAAM/popcorn-chewing.gif"
+        alt="gif popcorn"
+        width="250px"
+        height="200px"
+      />
+      <a href="/">Home</a>
+    </div>
+  );
+}
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,10 +1,17 @@
 import { Routes, Route } from "react-router";
 import { Home } from "../pages/Home";
+import { NotFound } from "../pages/NotFound";
+// import { Perfil } from "../pages/Perfil";
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" index element={<Home />} />
+      {/* <Route path="/about" index element={<Perfil />} /> */}
+      {/* <Route path="/education" index element={<Home />} />
+      <Route path="/experience" index element={<Home />} />
+      <Route path="/contact" index element={<Home />} /> */}
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
