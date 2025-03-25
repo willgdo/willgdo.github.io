6cb65544fdad1ccaea52c5074c355e7fee3ae473
Willian Oliveira <willian.oliveira737@gmail.com>
2025-03-25 12:00:00 -0300

    Remove comments

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -7,10 +7,6 @@ export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" index element={<Home />} />
-      {/* <Route path="/about" index element={<Perfil />} /> */}
-      {/* <Route path="/education" index element={<Home />} />
-      <Route path="/experience" index element={<Home />} />
-      <Route path="/contact" index element={<Home />} /> */}
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
