2984d952727b1d983a2dc5d1b573ca23a77a2a74
Willian Oliveira <willian.oliveira737@gmail.com>
2025-04-27 00:55:05 -0300

    Ajusta style Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,42 +6,44 @@ interface HeaderProps {
 
 function Header({ isMinimized }: HeaderProps) {
   return (
-    <>
-      <header className={isMinimized ? "header h-reduced" : "header"}>
-        <div className={isMinimized ? "perfil p-expanded" : "perfil"}>
-          <img src="./assets/photo.jpg" alt="Imagem de Willian Oliveira" />
-          <h2>
-            <strong>Willian Oliveira</strong>
-          </h2>
-          <h1>Software Developer</h1>
-          <p>
-            Desenvolvedor de software com 9+ anos de experiência, apaixonado por
-            tecnologia e sempre em busca de aprender algo novo. Tenho por
-            objetivo resolver problemas, ajudar negócios a crescer e criar
-            soluções que realmente façam sentido e deixem um impacto positivo.
-          </p>
-          <div className="tags">
-            <ul>
-              {stacks.map((stack) => {
-                return (
-                  <li key={stack}>
-                    <img
-                      src={
-                        stack === "jest"
-                          ? `https://raw.githubusercontent.com/devicons/devicon/master/icons/${stack}/${stack}-plain.svg`
-                          : `https://raw.githubusercontent.com/devicons/devicon/master/icons/${stack}/${stack}-original.svg`
-                      }
-                      alt={`${stack} Icon`}
-                      title={stack}
-                    />
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
+    <header className={isMinimized ? "header minimized" : "header"}>
+      <div
+        className={
+          isMinimized ? "header-container expanded" : "header-container"
+        }
+      >
+        <img src="./assets/photo.jpg" alt="Imagem de Willian Oliveira" />
+        <h2>
+          <strong>Willian Oliveira</strong>
+        </h2>
+        <h1>Software Developer</h1>
+        <p>
+          Desenvolvedor de software com 9+ anos de experiência, apaixonado por
+          tecnologia e sempre em busca de aprender algo novo. Tenho por objetivo
+          resolver problemas, ajudar negócios a crescer e criar soluções que
+          realmente façam sentido e deixem um impacto positivo.
+        </p>
+        <div className="stacks">
+          <ul>
+            {stacks.map((stack) => {
+              return (
+                <li key={stack}>
+                  <img
+                    src={
+                      stack === "jest"
+                        ? `https://raw.githubusercontent.com/devicons/devicon/master/icons/${stack}/${stack}-plain.svg`
+                        : `https://raw.githubusercontent.com/devicons/devicon/master/icons/${stack}/${stack}-original.svg`
+                    }
+                    alt={`${stack} Icon`}
+                    title={stack}
+                  />
+                </li>
+              );
+            })}
+          </ul>
         </div>
-      </header>
-    </>
+      </div>
+    </header>
   );
 }
 
